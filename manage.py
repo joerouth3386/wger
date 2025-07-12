08400ee44f1522d57d1c3c8398572db946b45d24
UncagedGrowth <53210141+joerouth3386@users.noreply.github.com>
2025-07-12 10:52:33 -0700

    Update manage.py
    git add manage.py
    git commit -m "Fix Django settings import path"
    git push

--- manage.py
+++ manage.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-# Standard Library
+import os
 import sys
-
-# Django
 from django.core.management import execute_from_command_line
 
+if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wger.settings")
+    execute_from_command_line(sys.argv)
+    
 # wger
 from wger.tasks import (
     get_path,
