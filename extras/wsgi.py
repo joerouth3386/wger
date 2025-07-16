d2ed480929a59079d09d22f88be4cf030807e6c4
UncagedGrowth <53210141+joerouth3386@users.noreply.github.com>
2025-07-16 07:28:17 -0700

    Update wsgi.py

--- extras/wsgi.py
+++ extras/wsgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wger.settings')  # use your path here
-
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wger.settings'  # Force set (override any default)
+os.environ['PYTHONPATH'] = os.path.dirname(os.path.abspath(__file__))  # Add dir of wsgi.py
 application = get_wsgi_application()
 __init__.py
