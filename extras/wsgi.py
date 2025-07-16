484ea586dd97422a0c51d4e3e41f7c2854a6eae5
UncagedGrowth <53210141+joerouth3386@users.noreply.github.com>
2025-07-16 07:30:37 -0700

    Update wsgi.py
    git add . && git commit -m "Fix settings import for runtime" && git push

--- extras/wsgi.py
+++ extras/wsgi.py
@@ -4,4 +4,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ['DJANGO_SETTINGS_MODULE'] = 'wger.settings'  # Force set (override any default)
 os.environ['PYTHONPATH'] = os.path.dirname(os.path.abspath(__file__))  # Add dir of wsgi.py
 application = get_wsgi_application()
+
 __init__.py
